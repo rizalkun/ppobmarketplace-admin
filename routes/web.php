ceea303e7d4139e02e1a6c93fca46668369f6018
Dimz99 <akucahgaul@gmail.com>
2022-09-02 16:05:47 +0700

    update 1.1

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('auth.forgot-password');
+    return redirect('login');
 });
 
 Route::get('/login', function () { return view('auth.login'); });
